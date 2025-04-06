ecaaa9ad4a7f27fa78dfaf53c1e066e64f3d3001
Kristian Cenov <40594589+kcenow@users.noreply.github.com>
2025-04-06 18:42:41 +0300

    Initalization

---
+++ Clear Icons Cache.bat
@@ -0,0 +1,52 @@
+@echo off
+rem [ SETTINGS ]==========================================================
+set AppName=Clear Icons Cache
+::set AppAuthor=Kristian Cenov
+::set AppVersion=v2025.04.06
+::set AppWeb=kcenow.com
+rem ======================================================================
+
+rem [ REGISTER ]==========================================================
+title %AppName%
+rem ======================================================================
+
+rem [ MAIN ]==============================================================
+:: Clear icons cache
+ie4uinit.exe -ClearIconCache
+
+:: Force kill File Explorer
+taskkill /f /im explorer.exe
+
+:: Delete icon cache database
+del /A /Q "%localappdata%\IconCache.db"
+del /A /Q /F "%localappdata%\Microsoft\Windows\Explorer\iconcache*"
+
+:: Start File Explorer
+start explorer.exe
+start .
+
+:: Show notification for clearing icon cache
+timeout 3
+call :fn_popup "Clear Icons Cache.exe" "Icon cache was cleared successfuly!" "Info"
+
+:: Force exit
+exit
+rem ======================================================================
+
+rem [ FUNCTIONS ]==========================================================
+:: *** Function PopUP v1.0 - Show notification popup ***
+:: Usage: call :fn_popup "Title" "Message" "Icon" "Time to show in seconds (Default: 0-Unlimited[OK button needs to be pressed])"
+:: Icon Types: None, Error, Question, Warning, Info
+:fn_popup
+if "%~1" == "" (set PopupTitle="Default Title") else (set PopupTitle=%1)
+if "%~2" == "" (set PopupMessage="Default Message") else (set PopupMessage=%2)
+if "%~3" == "" (set PopupIcon=0)
+if "%~3" == "None" (set PopupIcon=0)
+if "%~3" == "Error" (set PopupIcon=10)
+if "%~3" == "Question" (set PopupIcon=20)
+if "%~3" == "Warning" (set PopupIcon=30)
+if "%~3" == "Info" (set PopupIcon=40)
+if "%~4" == "" (set PopupTime=0) else (set PopupTime=%4)
+powershell -Command "(New-Object -ComObject Wscript.Shell).Popup('%PopupMessage%', %PopupTime%, '%PopupTitle%', 0x%PopupIcon%)"
+exit /B
+rem ======================================================================
--- README.md
+++ README.md
@@ -1,2 +1,34 @@
-# clear-icons-cache
- This is a portable application for windows, which allows you to clear icons cache with just one click. 
+<h1 align="center"><img src="https://raw.githubusercontent.com/kcenow/clear-icons-cache/main/icon.ico" width="35px" height="35px"> Clear Icons Cache</h1>
+<p align="center"><b><a href="#page_facing_up-description-">:page_facing_up:DESCRIPTION</a> • <a href="#cd-install-">:cd:INSTALL</a> • <a href="#arrow_down-download-">:arrow_down:DOWNLOAD</a> • <a href="#scroll-changelog-">:scroll:CHANGELOG</a> • <a href="#copyright-copyright-">:copyright:COPYRIGHT</a></b></p>
+
+<br />
+
+<h1>:page_facing_up: Description <a href="#-clear-icons-cache" title="Go to Navigation">[↑]</a></h1>
+
+* This is a simple application for windows, which allows you to clear icon cache with just one click.
+
+* File Explorer will be restarted and application will return you to the directory where it is located.
+
+<br />
+
+<h1>:cd: Install <a href="#-clear-icons-cache" title="Go to Navigation">[↑]</a></h1>
+
+NOT REQUIRED!
+
+<br />
+
+<h1>:arrow_down: Download <a href="#-clear-icons-cache" title="Go to Navigation">[↑]</a></h1>
+
+* <b>[Latest version](https://github.com/kcenow/clear-icons-cache/releases/tag/v2025.04.06 "Latest version")</b>
+* <b>[All versions](https://github.com/kcenow/clear-icons-cache/releases "All versions")</b>
+
+<br />
+
+<h1>:scroll: Changelog <a href="#-clear-icons-cache" title="Go to Navigation">[↑]</a></h1>
+
+<b>[v2025.04.06:](https://github.com/kcenow/clear-icons-cache/releases/tag/v2025.04.06 "Latest version")</b>	First Release.
+
+<br />
+
+<h1>:copyright: Copyright <a href="#-clear-icons-cache" title="Go to Navigation">[↑]</a></h1>
+© Kristian Cenov &#128640; KC. All rights reserved. This application is not affiliated with Windows® or © Microsoft Corporation. File Explorer® and all related elements are trademarks of and © Microsoft Corporation.
